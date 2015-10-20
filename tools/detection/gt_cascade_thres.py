fafab5dc09c74c711121df56e90d77d81f5c5a89
Kai KANG <myfavouritekk@gmail.com>
2015-10-20 17:38:07 +0800

    Fix matlab v7.3 error in gt_cascade_thres.py; fix possible error if none of the boxes covering gt.

--- tools/detection/gt_cascade_thres.py
+++ tools/detection/gt_cascade_thres.py
@@ -3,6 +3,7 @@
 import argparse
 import numpy as np
 import sys
+import h5py
 sys.path.insert(1, '.')
 from vdetlib.utils.common import iou
 import scipy.io as sio
@@ -16,16 +17,22 @@ if __name__ == '__main__':
 
     print "Processing {}".format(args.gt_boxfile)
     gt_boxes = sio.loadmat(args.gt_boxfile)['boxes']
-    alexnet_dets = sio.loadmat(args.alexnet_det_file)
-    boxes = alexnet_dets['boxes']
-    scores = alexnet_dets['zs']
+    try:
+        alexnet_dets = sio.loadmat(args.alexnet_det_file)['d']
+        boxes = alexnet_dets['boxes']
+        scores = alexnet_dets['zs']
+    except NotImplementedError:
+        alexnet_dets = h5py.File(args.alexnet_det_file)['d']
+        boxes = alexnet_dets['boxes'][:].T
+        scores = alexnet_dets['zs'][:].T
     max_scores = np.max(scores, axis = 1)
     overlaps = iou(gt_boxes, boxes)
 
     # true positive masks
     tp = overlaps >= 0.5
     # maxinum score from true positives for each gt box
-    thres = [np.max(max_scores[inds]) for inds in tp]
+    # yield -1000 if not any boxes covering the gt box
+    thres = [np.max(max_scores[inds]) if np.any(inds) else -1000 for inds in tp]
     sio.savemat(args.save_file,
         {'boxes': gt_boxes,
          'thresholds': thres}, do_compression=True)
