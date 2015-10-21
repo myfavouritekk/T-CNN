a802c9bb574748922a2b1c4617a2bb80400bab90
Kai KANG <myfavouritekk@gmail.com>
2015-10-21 20:54:38 +0800

    Improve speed of gt_cascade_thres.py upto 10x.

--- tools/detection/gt_cascade_thres.py
+++ tools/detection/gt_cascade_thres.py
@@ -4,10 +4,27 @@ import argparse
 import numpy as np
 import sys
 import h5py
-sys.path.insert(1, '.')
-from vdetlib.utils.common import iou
 import scipy.io as sio
 import os
+
+def iou(boxes1, boxes2):
+    boxes1 = np.asarray(boxes1)
+    boxes2 = np.asarray(boxes2)
+    # intersection boundaries, widths and heights
+    ix1 = np.maximum(boxes1[:,[0]], boxes2[:,[0]].T)
+    ix2 = np.minimum(boxes1[:,[2]], boxes2[:,[2]].T)
+    iy1 = np.maximum(boxes1[:,[1]], boxes2[:,[1]].T)
+    iy2 = np.minimum(boxes1[:,[3]], boxes2[:,[3]].T)
+    iw = np.maximum(0, ix2 - ix1 + 1)
+    ih = np.maximum(0, iy2 - iy1 + 1)
+    # areas
+    areas1 = (boxes1[:, [2]] - boxes1[:, [0]] + 1) * \
+             (boxes1[:, [3]] - boxes1[:, [1]] + 1)
+    areas2 = (boxes2[:, [2]] - boxes2[:, [0]] + 1) * \
+             (boxes2[:, [3]] - boxes2[:, [1]] + 1)
+    inter = iw * ih
+    overlaps = 1. * inter / (areas1 + areas2.T - inter)
+    return overlaps
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -16,19 +33,26 @@ if __name__ == '__main__':
     parser.add_argument('save_file')
     args = parser.parse_args()
 
+    if os.path.isfile(args.save_file):
+        print "{} already exists.".format(args.save_file)
+        sys.exit(0)
+
     print "Processing {}".format(args.gt_boxfile)
     gt_boxes = sio.loadmat(args.gt_boxfile)['boxes']
     if len(gt_boxes) == 0:
         print "No gt boxes in {}".format(args.gt_boxfile)
         sys.exit(0)
     try:
-        alexnet_dets = sio.loadmat(args.alexnet_det_file)['d']
-        boxes = alexnet_dets['boxes']
-        scores = alexnet_dets['zs']
+        alexnet_dets = sio.loadmat(args.alexnet_det_file)
+        boxes = alexnet_dets['d']['boxes']
+        scores = alexnet_dets['d']['zs']
     except NotImplementedError:
         alexnet_dets = h5py.File(args.alexnet_det_file)['d']
         boxes = alexnet_dets['boxes'][:].T
         scores = alexnet_dets['zs'][:].T
+    except KeyError:
+        boxes = alexnet_dets['boxes']
+        scores = alexnet_dets['zs']
     max_scores = np.max(scores, axis = 1)
     overlaps = iou(gt_boxes, boxes)
 
@@ -43,3 +67,5 @@ if __name__ == '__main__':
     sio.savemat(args.save_file,
         {'boxes': gt_boxes,
          'thresholds': thres}, do_compression=True)
+
+
