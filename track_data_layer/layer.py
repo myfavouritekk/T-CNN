e32859e8619a7d476c3b19d12c2c8933cbf9c064
Kai KANG <myfavouritekk@gmail.com>
2015-11-03 00:37:31 +0800

    Add features and 200-class scores in track_data_layer.

--- track_data_layer/layer.py
+++ track_data_layer/layer.py
@@ -4,6 +4,20 @@ import json
 import numpy as np
 import os
 import random
+import gzip
+
+def proto_load(file_path):
+    # load .gz version if exists
+    # AD_HOC implementation
+    if os.path.isfile(file_path + '.gz'):
+        file_path += '.gz'
+    if os.path.splitext(file_path)[1] == '.gz':
+        with gzip.GzipFile(file_path) as f:
+            obj = json.loads(f.read())
+    else:
+        with open(file_path, 'r') as f:
+            obj = json.load(f)
+    return obj
 
 
 class TrackDataLayer(caffe.Layer):
@@ -12,35 +26,47 @@ class TrackDataLayer(caffe.Layer):
     def _track_preprocess(self):
         tracks = []
         for score_file in self._score_files:
-            with open(score_file) as f:
-                score_proto = json.load(f)
-                for tubelet in score_proto['tubelets']:
-                    track = {}
-                    track['length'] = len(tubelet['boxes'])
-                    track['gt'] = tubelet['gt']
-                    track['mean_iou'] = np.mean([map(lambda x:x['gt_overlap'],
-                                              tubelet['boxes'])])
-                    track['det_scores'] = map(lambda x:x['det_score'],
-                                              tubelet['boxes'])
-                    track['track_scores'] = map(lambda x:x['track_score'],
-                                              tubelet['boxes'])
-                    track['anchors'] = map(lambda x:x['anchor'] * 1. / track['length'],
-                                              tubelet['boxes'])
-                    track['abs_anchors'] = map(abs, track['anchors'])
-                    track['gt_overlaps'] = map(lambda x:x['gt_overlap'],
-                                              tubelet['boxes'])
-                    tracks.append(track)
+            score_proto = proto_load(score_file)
+            for tubelet in score_proto['tubelets']:
+                track = {}
+                # general information
+                track['length'] = len(tubelet['boxes'])
+                track['gt'] = tubelet['gt']
+                track['mean_iou'] = np.mean([map(lambda x:x['gt_overlap'],
+                                          tubelet['boxes'])])
+
+                # tops
+                track['det_scores'] = map(lambda x:x['det_score'],
+                                          tubelet['boxes'])
+                track['track_scores'] = map(lambda x:x['track_score'],
+                                          tubelet['boxes'])
+                track['anchors'] = map(lambda x:x['anchor'] * 1. / track['length'],
+                                          tubelet['boxes'])
+                track['abs_anchors'] = map(abs, track['anchors'])
+                track['gt_overlaps'] = map(lambda x:x['gt_overlap'],
+                                          tubelet['boxes'])
+                track['labels'] = [1 if iou >= 0.5 else 0 for iou in track['gt_overlaps']]
+                # skip memory heavy features if possible
+                if 'all_scores' in self._top_names:
+                    track['all_scores'] = map(lambda x:x['all_score'],
+                                          tubelet['boxes'])
+                if 'feats' in self._top_names:
+                    track['feats'] = map(lambda x:x['feat'],
+                                          tubelet['boxes'])
+                tracks.append(track)
         return tracks
 
     def setup(self, bottom, top):
         layer_params = yaml.load(self.param_str)
         config = yaml.load(open(layer_params['config']).read())
 
-        assert len(top) >= 1 and len(top) <= 4
+        assert len(top) >= 1 and len(top) <= 6
         self._tot_top_names = [
             'det_scores',
             'track_scores',
-            'anchors'
+            'abs_anchors',
+            'all_scores',
+            'feats'
         ]
 
         # phase, current only support setting phase in config file, so be careful
@@ -66,8 +92,13 @@ class TrackDataLayer(caffe.Layer):
         self._track_index = range(len(self._tracks))
 
         # reshape top blobs
-        for top_id in xrange(len(top)):
-            top[top_id].reshape(self._batch_size, 1, self._length)
+        for top_id, top_name in zip(xrange(len(top)), self._top_names + ['labels']):
+            sample_feat = self._tracks[0][top_name][0]
+            if type(sample_feat) is list:
+                feat_length = len(sample_feat)
+            else:
+                feat_length = 1
+            top[top_id].reshape(self._batch_size, feat_length, 1, self._length)
 
 
     def _rotate_list(self, l, n):
@@ -75,6 +106,7 @@ class TrackDataLayer(caffe.Layer):
 
 
     def forward(self, bottom, top):
+        # select batch for different phases
         if self._phase == 'train':
             pos_index = np.random.choice(self._pos_index, size=self._num_pos).tolist()
             neg_index = np.random.choice(self._neg_index,
@@ -86,23 +118,27 @@ class TrackDataLayer(caffe.Layer):
             # test insequence
             batch_index = self._track_index[:self._batch_size]
             self._track_index = self._rotate_list(self._track_index, self._batch_size)
+
+        # prepare batch
         for num_id, track_id in enumerate(batch_index):
             track = self._tracks[track_id]
             # sample starting point, 0 if track length is too short
             st_index = random.randint(0, max(0, track['length'] - self._length))
             valid_length = min(track['length'], self._length)
-            # for blob_name in ['det_scores', 'track_scores', 'anchors']:
             for blob_name in self._top_names:
                 top_id = self._name_to_top_map[blob_name]
                 data = np.zeros_like(top[top_id].data[num_id])
-                data[0,0:valid_length] = \
-                    track[blob_name][st_index:st_index+valid_length]
+                feat_length = data.shape[0]
+                orig_data = np.asarray(track[blob_name]).reshape((feat_length, 1, -1))
+                # data[:, 0, 0:valid_length] = \
+                #     track[blob_name][st_index:st_index+valid_length]
+                data[:, :, 0:valid_length] = orig_data[:, :, st_index:st_index+valid_length]
                 top[top_id].data[num_id] = data.astype(np.float32, copy=True)
             # use -1 as ignored label
             top_label_id = self._name_to_top_map['labels']
             labels = -np.ones_like(top[top_label_id].data[num_id])
-            overlaps = np.asarray(track['gt_overlaps'][st_index:st_index+valid_length])
-            labels[0, 0:valid_length] = (overlaps > 0.5).astype('int')
+            valid_labels = np.asarray(track['labels'][st_index:st_index+valid_length])
+            labels[0, 0, 0:valid_length] = valid_labels
             top[top_label_id].data[num_id] = labels.astype(np.float32, copy=True)
 
     def reshape(self, bottom, top):
