f2176ff85b884fccd218da0aa1d81ecc042c1146
Kai KANG <myfavouritekk@gmail.com>
2015-11-14 15:25:12 +0800

    Enable show_score_proto to support both nms and non-nms options.

--- tools/visual/show_score_proto.py
+++ tools/visual/show_score_proto.py
@@ -6,8 +6,8 @@ import cv2
 import numpy as np
 import sys
 sys.path.insert(1, '.')
-from vdetlib.utils.protocol import proto_load, frame_path_at
-from vdetlib.utils.cython_nms import vid_nms
+from vdetlib.utils.protocol import proto_load, frame_path_at, tubelet_box_proto_at_frame
+from vdetlib.utils.cython_nms import nms
 from vdetlib.utils.visual import add_bbox
 from vdetlib.utils.common import imread, imwrite
 
@@ -23,33 +23,14 @@ if __name__ == '__main__':
     parser.add_argument('score_file')
     parser.add_argument('--varname')
     parser.add_argument('--save_dir', default=None)
+    parser.add_argument('--nms', dest='do_nms', action='store_true')
+    parser.set_defaults(do_nms=False)
     args = parser.parse_args()
 
     vid_proto = proto_load(args.vid_file)
     score_proto = proto_load(args.score_file)
     vid_name = vid_proto['video']
     assert vid_name == score_proto['video']
-
-    dets = []
-    for tubelet in score_proto['tubelets']:
-        if tubelet['gt'] == 1:
-            raise ValueError('Dangerous: Score file contains gt tracks!')
-        class_index = tubelet['class_index']
-        for box in tubelet['boxes']:
-            frame_idx = box['frame']
-            image_name = image_name_at_fame(vid_proto, frame_idx)
-            bbox = map(lambda x:max(x,0), box['bbox'])
-            score = box[args.varname]
-            dets.append([int(frame_idx), class_index, score, bbox])
-
-    nms_boxes = [[det[0],]+det[-1]+[det[2],] for det in dets]
-    keep = vid_nms(np.asarray(nms_boxes).astype('float32'), 0.3)
-
-    kept_dets = [dets[i] for i in keep]
-    # for frame_idx, class_index, score, bbox in kept_dets:
-    #     print '{} {} {:.6f} {:.2f} {:.2f} {:.2f} {:.2f}'.format(
-    #         frame_idx, class_index, score,
-    #         bbox[0], bbox[1], bbox[2], bbox[3])
 
     for frame in vid_proto['frames']:
         if args.save_dir:
@@ -60,7 +41,17 @@ if __name__ == '__main__':
                 continue
         img = imread(frame_path_at(vid_proto, frame['frame']))
         frame_idx = frame['frame']
-        boxes = [det[3] for det in kept_dets if det[0] == frame_idx]
+        tubelet_boxes = map(lambda x:tubelet_box_proto_at_frame(x, frame_idx), score_proto['tubelets'])
+        if args.do_nms:
+            valid_idx = [i for i, box in enumerate(tubelet_boxes) if box is not None]
+            valid_boxes = [tubelet_boxes[i]['bbox'] for i in valid_idx]
+            valid_scores = [tubelet_boxes[i][args.varname] for i in valid_idx]
+            valid_box_score = [tubelet_boxes[i]['bbox']+[tubelet_boxes[i][args.varname],] for i in valid_idx]
+            kept_index = nms(np.asarray(valid_box_score, dtype='float32'), 0.3)
+            kept_orig_idx = [valid_idx[i] for i in kept_index]
+            boxes = [tubelet_boxes[i]['bbox'] for i in kept_orig_idx]
+        else:
+            boxes = [x['bbox'] if x is not None else None for x in tubelet_boxes]
         tracked = add_bbox(img, boxes, None, 10)
         if args.save_dir:
             if not os.path.isdir(args.save_dir):
