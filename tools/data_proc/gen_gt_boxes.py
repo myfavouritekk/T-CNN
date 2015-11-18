2392449a8e5cb3ca074c9b71a53771ae1c0d83ee
Kai KANG <myfavouritekk@gmail.com>
2015-11-19 01:47:28 +0800

    Add script to save ground truth boxes.

---
+++ tools/data_proc/gen_gt_boxes.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import argparse
+import json
+import os
+import scipy.io as sio
+import numpy as np
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('vid_file')
+    parser.add_argument('annot_file')
+    parser.add_argument('save_dir')
+    args = parser.parse_args()
+
+    with open(args.vid_file) as f:
+        vid_proto = json.load(f)
+    with open(args.annot_file) as f:
+        annot_proto = json.load(f)
+
+    if not os.path.isdir(args.save_dir):
+        os.makedirs(args.save_dir)
+
+    assert vid_proto['video'] == annot_proto['video']
+    video = vid_proto['video']
+    print "Processing {}".format(video)
+    tracks = [annot['track'] for annot in annot_proto['annotations']]
+    for frame in vid_proto['frames']:
+        save_name = os.path.splitext(os.path.basename(frame['path']))[0]
+        boxes = [box['bbox'] for track in tracks for box in track if box['frame'] == frame['frame']]
+        save_file = os.path.join(args.save_dir, save_name + '.mat')
+        sio.savemat(save_file, {'boxes': np.asarray(boxes, dtype='float64')},
+            do_compression=True)
