7a1ef9e517a9eb7954744ee9bc90d17482c11680
Kai KANG <myfavouritekk@gmail.com>
2015-11-19 18:52:43 +0800

    Add height and width to gt boxes.

--- tools/data_proc/gen_gt_boxes.py
+++ tools/data_proc/gen_gt_boxes.py
@@ -5,6 +5,11 @@ import json
 import os
 import scipy.io as sio
 import numpy as np
+import cv2
+
+def frame_path_at(vid_proto, frame_id):
+    frame = [frame for frame in vid_proto['frames'] if frame['frame'] == frame_id][0]
+    return str(os.path.join(vid_proto['root_path'], frame['path']))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -27,10 +32,20 @@ if __name__ == '__main__':
     tracks = [annot['track'] for annot in annot_proto['annotations']]
     for frame in vid_proto['frames']:
         save_name = os.path.splitext(os.path.basename(frame['path']))[0]
-        boxes_cls = [[box['bbox'], box['class_index']] for track in tracks for box in track if box['frame'] == frame['frame']]
+        boxes_cls_size = [[box['bbox'], box['class_index'], box['frame_size']] for track in tracks for box in track if box['frame'] == frame['frame']]
         save_file = os.path.join(args.save_dir, save_name + '.mat')
-        boxes = map(lambda x:x[0], boxes_cls)
-        classes = map(lambda x:x[1], boxes_cls)
+        boxes = map(lambda x:x[0], boxes_cls_size)
+        classes = map(lambda x:x[1], boxes_cls_size)
+        frame_size = map(lambda x:x[2], boxes_cls_size)
+        if boxes_cls_size is not []:
+            height = frame_size[0][0]
+            width = frame_size[0][1]
+        else:
+            frame_path = frame_path_at(vid_proto, frame['frame'])
+            height, width = cv2.imread(frame_path).shape[:2]
         sio.savemat(save_file, {'boxes': np.asarray(boxes, dtype='float64'),
-                                'labels': np.asarray(classes, dtype='float64')},
+                                'labels': np.asarray(classes, dtype='float64'),
+                                'channels': 3,
+                                'height': height,
+                                'width': width},
             do_compression=True)
